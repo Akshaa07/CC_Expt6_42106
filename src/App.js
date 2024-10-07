9985ade0e1d73101657c91c66920fa13034d80d9
Akanksha <phadatareakanksha7@gmail.com>
2024-10-08 00:49:18 +0530

    Ok

---
+++ src/App.js
@@ -0,0 +1,31 @@
+// import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+        <p>
+          Aman Choudhari
+        </p>
+        <p>
+          42411
+        </p>
+        <p>
+          P-8
+        </p>
+        {/* <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a> */}
+      </header>
+    </div>
+  );
+}
+
+export default App;
